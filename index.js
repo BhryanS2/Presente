2b8a3aada4e983c52e3b07c8b8c0822e9e98a620
BhryanS2 <bhryanstepenhen@gmail.com>
2023-12-05 21:36:43 -0300

    fix: remove envents

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const heart = document.querySelector(".heart");
 
 
 heart.addEventListener("click", () => {
+  heart.classList.toggle("active");
+
+  const parabens = document.querySelector(".parabens");
+
+  const frasesRomanticas = [
+    "Parabéns meu amor, você é a melhor coisa que aconteceu na minha vida!!",
+    "Eu te amo muito",
+    "Você é a pessoa mais importante da minha vida",
+    "<3"
+  ]
+
+  parabens.innerHTML = frasesRomanticas[Math.floor(Math.random() * frasesRomanticas.length)];
+
+  setTimeout(() => {
     heart.classList.toggle("active");
-    setTimeout(() => {
-        heart.classList.toggle("active");
-    }, 2000);
-    
+  }, 2000);
+
 })
